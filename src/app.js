f09ae76c5939fdd9b888b2001c5a1e956b1d1111
Jhowmsm <jhownsman@gmail.com>
2024-08-28 14:24:16 +0000

    refactorizacion

--- src/app.js
+++ src/app.js
@@ -5,36 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-let ext = [".com", ".net"];
-
 // Función analizar todas las posibles combinaciones
-function retornarTodasLasCombinaciones() {
-  let combinaciones = [];
+function returnallcombinations() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".net"];
+
+  let combinations = [];
 
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
         for (let l = 0; l < ext.length; l++) {
-          combinaciones.push(`${pronoun[i]}${adj[j]}${noun[k]}${ext[l]}`);
+          combinations.push(`${pronoun[i]}${adj[j]}${noun[k]}${ext[l]}`);
         }
       }
     }
   }
-  return combinaciones;
+  return combinations;
 }
 
 // Función elegir una combinacion aleatoria de todas las posibles
-function obtenerLineaAleatoria() {
-  let combinaciones = retornarTodasLasCombinaciones();
-  let aleatorio = Math.floor(Math.random() * combinaciones.length);
-  return combinaciones[aleatorio];
+function getRandomLine() {
+  let combinations = returnallcombinations();
+  let random = Math.floor(Math.random() * combinations.length);
+  return combinations[random];
 }
 
 // Obtener una combinación aleatoria
-let lineaAleatoria = obtenerLineaAleatoria();
+let randomLine = getRandomLine();
 
 // Mostrar la combinación aleatoria en el HTML
-document.getElementById("resultado").innerHTML = lineaAleatoria;
+document.getElementById("show").innerHTML = randomLine;
